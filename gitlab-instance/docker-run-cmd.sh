269b800039073b616c07112fd08cbb19badc5fdc
Kabir <45986208+kabirg@users.noreply.github.com>
2020-05-16 19:49:07 -0400

    Update docker-run-cmd.sh

--- gitlab-instance/docker-run-cmd.sh
+++ gitlab-instance/docker-run-cmd.sh
@@ -1,6 +1,7 @@
 sudo docker run --detach \
   --hostname [YOUR_PUBLIC_IP] \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://[YOUR_PUBLIC_IP]'; gitlab_rails['gitlab_shell_ssh_port'] = 2222;" \
+  --publish 443:443 --publish 80:80 --publish 2222:22 \
   --name gitlab \
   --restart always \
   --volume srv/gitlab/config:/etc/gitlab \
