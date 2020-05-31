266ccb1e0cc44d2f1fdf5e2d8d1cb45c85699802
Kabir <kabirgupta@live.com>
2020-05-31 12:17:22 -0400

    Updating simple-pipeline with a new basic pipeline that builds/deploys a CFN_NAG image to ECR

--- gitlab-instance/docker-run-cmd.sh
+++ gitlab-instance/docker-run-cmd.sh
@@ -1,10 +1,10 @@
 sudo docker run --detach \
-  --hostname [YOUR_PUBLIC_IP] \
-  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://[YOUR_PUBLIC_IP]'; gitlab_rails['gitlab_shell_ssh_port'] = 2222;" \
+  --hostname YOUR_PUBLIC_IP \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://YOUR_PUBLIC_IP'; gitlab_rails['gitlab_shell_ssh_port'] = 2222;" \
   --publish 443:443 --publish 80:80 --publish 2222:22 \
   --name gitlab \
   --restart always \
-  --volume srv/gitlab/config:/etc/gitlab \
-  --volume srv/gitlab/logs:/var/log/gitlab \
-  --volume srv/gitlab/data:/var/opt/gitlab \
+  --volume /srv/gitlab/config:/etc/gitlab \
+  --volume /srv/gitlab/logs:/var/log/gitlab \
+  --volume /srv/gitlab/data:/var/opt/gitlab \
   gitlab/gitlab-ce:latest
--- simple-pipeline/README.md
+++
@@ -1,2 +0,0 @@
-Reference:
-https://blog.ruanbekker.com/blog/2018/12/19/setup-a-basic-ci-pipeline-on-gitlab/
